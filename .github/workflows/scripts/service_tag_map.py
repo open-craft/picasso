93b4ee670597d39c2fc760329ebe544510e80b2a
Agrendalath <piotr@surowiec.it>
2026-05-04 00:21:11 +0200

    feat: add more services to the Docker image mapping

--- .github/workflows/scripts/service_tag_map.py
+++ .github/workflows/scripts/service_tag_map.py
@@ -12,5 +12,16 @@ Values:
 service_tag_map = {
   "openedx": "DOCKER_IMAGE_OPENEDX",
   "mfe": "MFE_DOCKER_IMAGE",
-  "aspects-superset": "DOCKER_IMAGE_SUPERSET"
+  "aspects-superset": "DOCKER_IMAGE_SUPERSET",
+  "discovery": "DISCOVERY_DOCKER_IMAGE",
+  "ecommerce": "ECOMMERCE_DOCKER_IMAGE",
+  "ecommerce-worker": "ECOMMERCE_WORKER_DOCKER_IMAGE",
+  "enterprise-catalog": "ENTERPRISE_CATALOG_DOCKER_IMAGE",
+  "enterprise-catalog-worker": "ENTERPRISE_CATALOG_WORKER_DOCKER_IMAGE",
+  "enterprise-access": "ENTERPRISE_ACCESS_DOCKER_IMAGE",
+  "enterprise-access-worker": "ENTERPRISE_ACCESS_WORKER_DOCKER_IMAGE",
+  "enterprise-subsidy": "ENTERPRISE_SUBSIDY_DOCKER_IMAGE",
+  "license-manager": "LICENSE_MANAGER_DOCKER_IMAGE",
+  "license-manager-worker": "LICENSE_MANAGER_WORKER_DOCKER_IMAGE",
+  "license-manager-bulk-enrollment-worker": "LICENSE_MANAGER_BULK_ENROLLMENT_WORKER_DOCKER_IMAGE",
 }
